4b74f575d303550c09f0271eae39c8ee671dca53
prachu12 <88527681+prachu12@users.noreply.github.com>
2022-07-28 20:05:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,49 @@
-import React, { useState } from "react";
-import "../styles/App.css";
+import React, { Component, useState, useEffect } from "react";
+import '../styles/App.css';
 
+let timer;
 const App = () => {
-  let [count, setCount] = useState(0);
+  // write your code here 
+  const [inputTime, setInputTime] = useState(-1);
+
+  const startTime = (event) => {
+    if (event.key === "Enter") {
+      if(timer){
+        clearInterval(timer);
+      }
+      if(isNaN(event.target.value)){
+        setInputTime(0);
+        return;
+      }
+      setInputTime(Math.floor(event.target.value))
+      startTimer();
+    }
+  }
+
+  useEffect(()=>{
+    if(inputTime < 1){
+      clearInterval(timer);
+    }
+  }, [inputTime])
+
+  function startTimer(){
+    timer = setInterval(()=>{
+      setInputTime(prevTime => prevTime - 1)
+    }, 1000)
+  }
+
 
   return (
-    <div className="ball">
-      <h1
-        className="count"
-        onDoubleClick={() => {
-          alert("cant edit it");
-        }}
-      >
-        {count}
-      </h1>
-      <button
-        className="increment-button"
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        Increment
-      </button>
+    <div className="wrapper">
+      <div id="whole-center">
+        <h1>
+          Reverse countdown for<input id="timeCount" onKeyDown={startTime} /> sec.
+        </h1>
+      </div>
+      <div id="current-time">{(inputTime != -1) ? inputTime : ""}</div>
     </div>
-  );
-};
+  )
+}
+
 
 export default App;
